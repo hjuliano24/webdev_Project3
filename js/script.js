c5e6d8a49c7a1679e6f9c4be0bed0ea59845d056
hjuliano24 <hjuliano@hawk.iit.edu>
2024-11-11 15:41:55 -0600

    Added google maps API code and redid javascript for trees

--- js/script.js
+++ js/script.js
@@ -4,58 +4,36 @@
 
 
 //For trees slider
-let slideIndex = 0;
-showSlides(); // call showslide method
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
-    let i;
-
-    // get the array of divs' with classname image-sliderfade
-    let slides = document.getElementsByClassName("image-sliderfade");
-
-    // get the array of divs' with classname dot
-    let dots = document.getElementsByClassName("dot");
-
-    for (i = 0; i < slides.length; i++) {
-        // initially set the display to
-        // none for every image.
-        slides[i].style.display = "none";
-    }
-
-    // increase by 1, Global variable
-    slideIndex++;
-
-    // check for boundary
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-
-    // Change image every 2 seconds
-    setTimeout(showSlides, 2000);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
 
 //for the API maps
 
 
-const CONFIGURATION = {
-        "locations": [
-          {"title":"Illinois Institute of Technology Academic Campus","address1":"3300 S Federal St","address2":"Chicago, IL 60616, USA","coords":{"lat":41.83446952994897,"lng":-87.62930912209013},"placeId":"ChIJxTColxIsDogR1I20Cuv5mag"}
-        ],
-        "mapOptions": {"center":{"lat":38.0,"lng":-100.0},"fullscreenControl":true,"mapTypeControl":false,"streetViewControl":false,"zoom":4,"zoomControl":true,"maxZoom":17,"mapId":""},
-        "mapsApiKey": "YOUR_API_KEY_HERE",
-        "capabilities": {"input":false,"autocomplete":false,"directions":false,"distanceMatrix":false,"details":false,"actions":false}
-      };
-      
-//<script type="module">
-      document.addEventListener('DOMContentLoaded', async () => {
-        await customElements.whenDefined('gmpx-store-locator');
-        const locator = document.querySelector('gmpx-store-locator');
-        locator.configureFromQuickBuilder(CONFIGURATION);
-      });
