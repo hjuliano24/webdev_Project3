b5ad6f4ccbca6577c7990592abf39958f46c2771
hjuliano24 <hjuliano@hawk.iit.edu>
2024-11-12 10:22:57 -0600

    Google api is working

--- js/script.js
+++ js/script.js
@@ -2,6 +2,38 @@
 //Make sure your JavaScript throws no uncaught errors and is loaded unobtrusively. 
 //You can check for errors using the console on your browser inspector
 
+//for the API maps
+// Initialize and add the map
+// Initialize and add the map
+function init() {
+
+  async function initMap() {
+  // The location of Uluru
+  const position = {lat: 41.8367, lng: -87.6260};
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 15,
+    center: position,
+    mapId: "befb6f94c2eb29a2",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Illinois Institute of Technology",
+  });
+}
+
+initMap();
+}
+
+window.addEventListener("load", init);
 
 //For trees slider
 let slideIndex = 1;
@@ -34,19 +66,3 @@ function showSlides(n) {
 }
 
 
-//for the API maps
-
-const CONFIGURATION = {
-  "locations": [
-  {"title":"Illinois Institute of Technology Academic Campus","address1":"3300 S Federal St","address2":"Chicago, IL 60616, USA","coords":{"lat":41.83446952994897,"lng":-87.62930912209013},"placeId":"ChIJxTColxIsDogR1I20Cuv5mag"}
-  ],
-  "mapOptions": {"center":{"lat":38.0,"lng":-100.0},"fullscreenControl":true,"mapTypeControl":false,"streetViewControl":false,"zoom":4,"zoomControl":true,"maxZoom":17,"mapId":""},
-  "mapsApiKey": "AIzaSyAbsqKRJPTslWJpzw06HlUg16ECouYTk4E",
-  "capabilities": {"input":false,"autocomplete":false,"directions":false,"distanceMatrix":false,"details":false,"actions":false}
-};
-
-document.addEventListener('DOMContentLoaded', async () => {
-  await customElements.whenDefined('gmpx-store-locator');
-  const locator = document.querySelector('gmpx-store-locator');
-  locator.configureFromQuickBuilder(CONFIGURATION);
-});
